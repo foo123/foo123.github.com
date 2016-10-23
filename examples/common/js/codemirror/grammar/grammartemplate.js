27c4a635915fed13e9399386e6325a91f5cb653b
Nikos M <nikosms0@gmail.com>
2016-10-23 18:02:00 +0300

    update

--- examples/common/js/codemirror/grammar/grammartemplate.js
+++ examples/common/js/codemirror/grammar/grammartemplate.js
@@ -18,21 +18,22 @@ var grammartemplate_grammar = {
 // Lexical model
 "Lex"                           : {
 
-     //"escaped"                  : "RE::/(\\\\\\\\)*\\\\/"
-     "modifier"                 : "RE::/(\\*|\\?!|\\?|\\{\\d+(,\\d+)?\\})/"
+     "modifier"                 : "RE::/(\\?!|\\*|\\?|\\{\\d+(,\\d+)?\\})/"
     ,"identifier"               : "RE::/[_A-Za-z][_A-Za-z0-9]*(\\.[_A-Za-z][_A-Za-z0-9]*)?/"
-    ,"identifier2"              : "RE::/[_A-Za-z][_A-Za-z0-9]*/"
-    ,"value"                    : "RE::/[_A-Za-z0-9]*/"
+    ,"renderer"                 : "RE::/[_A-Za-z][_A-Za-z0-9]*/"
+    ,"default_value"            : "RE::/[_A-Za-z0-9]*/"
     ,"text"                     : "RE::/[^\\s]/"
-    ,"delim1"                   : "RE::/(<|&lt;)/"
-    ,"delim2"                   : "RE::/(>|&gt;)(:=)?/"
+    ,"delim1"                   : "RE::/<|&lt;/"
+    ,"delim2"                   : "RE::/>|&gt;(:=)?/"
+    ,"block1"                   : "RE::/\\[/"
+    ,"block2"                   : "RE::/\\]/"
     ,"opened:action"            : {"push":">","ci":true}
     ,"closed:action"            : {"pop":">","ci":true,"msg":"Non-Terminal delimiters do not match"}
 },
 
 "Syntax"                        : {
-     "nonterminal"              : "delim1.keyword '' (modifier.function? identifier.keyword '')? (':' '' identifier2.function '')? ('|' '' value.string '')? delim2.keyword"
-    ,"template"                 : "nonterminal | text"
+     "nonterminal"              : "delim1.keyword '' (modifier.function? identifier.keyword '')? (':' '' renderer.function '')? ('|' '' default_value.string '')? delim2.keyword"
+    ,"template"                 : "nonterminal | block1.function | block2.function | text"
 },
 
 // what to parse and in what order
