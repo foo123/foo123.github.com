421972d66e5ed7037d26521315ec5bd4eb96e042
Nikos M <workingclasscode@gmail.com>
2021-08-11 20:32:01 +0300

    index, weather-react, weather-vue demos

---
+++ examples/weather-react/precache-manifest.2f4342c3bacfce3d66a0f5595e9a820a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "304fd4861e941edd60b6",
+    "url": "./static/css/main.7ef7ebb6.chunk.css"
+  },
+  {
+    "revision": "304fd4861e941edd60b6",
+    "url": "./static/js/main.ce155dcb.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1b072ef9f99246b65a68",
+    "url": "./static/js/2.8c4598b4.chunk.js"
+  },
+  {
+    "revision": "ae447b67e05129d1bd097a55cbdc9312",
+    "url": "./index.html"
+  }
+];
---
+++ examples/weather-react/precache-manifest.9dce34ce4478d3c77b12bc4407e43862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22fb8f80a9828e3a7c44",
+    "url": "/examples/weather-react/static/css/main.7ef7ebb6.chunk.css"
+  },
+  {
+    "revision": "22fb8f80a9828e3a7c44",
+    "url": "/examples/weather-react/static/js/main.617e60ac.chunk.js"
+  },
+  {
+    "revision": "d60c25f03fb631ac191b",
+    "url": "/examples/weather-react/static/js/runtime~main.b47d24ff.js"
+  },
+  {
+    "revision": "1b072ef9f99246b65a68",
+    "url": "/examples/weather-react/static/js/2.8c4598b4.chunk.js"
+  },
+  {
+    "revision": "9f5ba8fd041b5a6313adc7832ff3d902",
+    "url": "/examples/weather-react/index.html"
+  }
+];
