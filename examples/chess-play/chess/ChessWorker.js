8270d62729a2f5bd96934309b0485caa3753548e
Nikos M <workingclasscode@gmail.com>
2026-04-28 11:18:04 +0300

    chess

--- examples/chess-play/chess/ChessWorker.js
+++ examples/chess-play/chess/ChessWorker.js
@@ -3,20 +3,20 @@
 importScripts('./ChessGame.js');
 importScripts('./ChessSearch.js');
 
-var _stopped = false;
+var stopped = false;
 
 onmessage = function(e) {
     if (e.data.bestmove && e.data.fen && e.data.opts)
     {
         var game = new ChessGame(e.data.fen),
             opts = "string" === typeof e.data.opts ? JSON.parse(e.data.opts) : e.data.opts;
-        opts.stopped = function() {return _stopped;};
-        opts.cb = function(move) {game.dispose(); postMessage({move:move});};
-        _stopped = false;
+        opts.stopped = function() {return stopped;};
+        opts.cb = function(move) {game.dispose(); postMessage({move:stopped ? null : move});};
+        stopped = false;
         (new ChessSearch.HybridSearch(game, opts)).bestMove(game.getBoard().turn);
     }
     else if (e.data.stop)
     {
-        _stopped = true;
+        stopped = true;
     }
 };
